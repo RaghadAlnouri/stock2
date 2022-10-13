97e005f92ab6a15647442d15ec389d46c842c40b
Michael Al Tork <michael.altork@gmail.com>
2022-10-13 17:02:27 -0400

    add __main__ to app.py

--- app.py
+++ app.py
@@ -2,8 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def hello():
     return f'Flask has opened a server, try "/another_page"!\n'
 
-@app.route('/another_page', methods=['GET'])
+
+@app.route("/another_page", methods=["GET"])
+def another_page():
+    return f"here is another page"
+
+
+if __name__ == "__main__":
+    # When running python app.py this will run
+    # on the cloud a webserver such as Gunicorn will serve the app
+    app.run(host="localhost", port=8080, debug=True)
+
