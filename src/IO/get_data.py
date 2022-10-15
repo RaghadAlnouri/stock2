f09145787e8c18b899fde3d38b903daa0487ba67
Michael Al Tork <michael.altork@gmail.com>
2022-10-15 15:05:39 -0400

    add type hints to data fetcher

--- src/IO/get_data.py
+++ src/IO/get_data.py
@@ -1,12 +1,15 @@
+from typing import Callable
 import yahoo_fin.stock_info as si
 import datetime
+import pandas as pd
 
 
-def create_data_fetcher(last: bool = False):
-    def data_fetcher(ticker: str):
+def create_data_fetcher(last: bool = False) -> Callable[[str], pd.core.frame.DataFrame]:
+    def data_fetcher(ticker: str) -> pd.core.frame.DataFrame:
         if last:
             now = datetime.datetime.now()
             start_date = now - datetime.timedelta(days=30)
             return si.get_data(ticker, start_date)
         return si.get_data(ticker)
+
     return data_fetcher
