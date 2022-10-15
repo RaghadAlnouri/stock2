7bf88b69ba161ec08ad5d8f73158bddc67133443
Michael Al Tork <michael.altork@gmail.com>
2022-10-15 15:02:54 -0400

    add imports from other files

--- src/business_logic/process_query.py
+++ src/business_logic/process_query.py
@@ -1,3 +1,13 @@
-def predict(ticker: str) -> float:
-    return f"this is a prediction"
+import pandas as pd
+import numpy as np
+
+from src.algo.add_features import (
+    create_lag_creator,
+    create_cols_to_keep,
+    add_label_buy_close,
+    split_X_Y,
+    remove_nans,
+)
+from src.algo.create_model import create_pipeline, create_logistic_regression_learner
+from src.IO.get_data import create_data_fetcher
 
