f879d54ea87a89f5e1a3c2b4b9db4240ed88270c
Michael Al Tork <michael.altork@gmail.com>
2022-10-15 15:18:07 -0400

    business logic add pipeline get data add lags

--- src/IO/get_data.py
+++ src/IO/get_data.py
@@ -4,11 +4,13 @@ import datetime
 import pandas as pd
 
 
-def create_data_fetcher(last: bool = False) -> Callable[[str], pd.core.frame.DataFrame]:
+def create_data_fetcher(
+    lags: int, last: bool = False
+) -> Callable[[str], pd.core.frame.DataFrame]:
     def data_fetcher(ticker: str) -> pd.core.frame.DataFrame:
         if last:
             now = datetime.datetime.now()
-            start_date = now - datetime.timedelta(days=30)
+            start_date = now - datetime.timedelta(days=lags)
             return si.get_data(ticker, start_date)
         return si.get_data(ticker)
 
--- src/business_logic/process_query.py
+++ src/business_logic/process_query.py
@@ -11,3 +11,45 @@ from src.algo.add_features import (
 from src.algo.create_model import create_pipeline, create_logistic_regression_learner
 from src.IO.get_data import create_data_fetcher
 
+NUM_LAGS = 5
+
+# create training preprocessing pipeline
+def create_preprocess_pipeline_train():
+    preprocess_pipeline_train = create_pipeline(
+        create_data_fetcher(NUM_LAGS),
+        create_lag_creator(NUM_LAGS, "close"),
+        add_label_buy_close,
+        remove_nans,
+        create_cols_to_keep(
+            [
+                "close",
+                "close_lag1",
+                "close_lag2",
+                "close_lag3",
+                "close_lag4",
+                "close_lag5",
+                "label",
+            ]
+        ),
+    )
+    return preprocess_pipeline_train
+
+
+# create prediction preprocessing pipeline
+def create_preprocess_pipeline_predict():
+    preprocess_pipeline_predict = create_pipeline(
+        create_data_fetcher(NUM_LAGS, last=True),
+        create_lag_creator(NUM_LAGS, "close"),
+        remove_nans,
+        create_cols_to_keep(
+            [
+                "close",
+                "close_lag1",
+                "close_lag2",
+                "close_lag3",
+                "close_lag4",
+                "close_lag5",
+            ]
+        ),
+    )
+    return preprocess_pipeline_predict
