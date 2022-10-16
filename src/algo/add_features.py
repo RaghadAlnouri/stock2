b0d815824d60f9d2ca98f301158b6ba9ce76dc3a
Michael Al Tork <michael.altork@gmail.com>
2022-10-16 14:32:11 -0400

    change split_X_Y function to a create splitter

--- src/algo/add_features.py
+++ src/algo/add_features.py
@@ -21,16 +21,12 @@ def create_lag_creator(num_lags: int, col_name: str) -> Callable[[pd.core.frame.
 # function to add the label
 def add_label_buy_close(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     df["tomorrow_close"] = df.loc[:, "close"].shift(-1)
-    df["label"] = np.where(
-        df.loc[:, "close"] >= df.loc[:, "tomorrow_close"], "SELL", "BUY"
-    )
+    df["label"] = np.where(df.loc[:, "close"] >= df.loc[:, "tomorrow_close"], "SELL", "BUY")
     return df.drop("tomorrow_close", axis=1)
 
 
 # function to return a dataframe with the columns we want to keep for training
-def create_cols_to_keep(
-    list_cols: List[str],
-) -> Callable[[pd.core.frame.DataFrame], pd.core.frame.DataFrame]:
+def create_cols_to_keep(list_cols: List[str]) -> Callable[[pd.core.frame.DataFrame], pd.core.frame.DataFrame]:
     def cols_to_keep(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
         return df[list_cols]
 
@@ -38,13 +34,13 @@ def create_cols_to_keep(
 
 
 # function to split x and y for model
-def split_X_Y(
-    df: pd.core.frame.DataFrame,
-) -> Tuple[pd.core.frame.DataFrame, pd.core.frame.Series]:
-    df = df.copy()
-    X = df.drop("label", axis=1)
-    Y = df.iloc[:, -1]
-    return X, Y
+def create_splitter(col_label: str) -> Callable[[pd.core.frame.DataFrame],Tuple[pd.core.frame.DataFrame, pd.core.frame.Series]]
+    def split_x_y(df: pd.core.frame.DataFrame) -> Tuple[pd.core.frame.DataFrame, pd.core.frame.Series]:
+        df = df.copy()
+        x = df.drop(col_label, axis=1)
+        y = df[col_label]
+        return x, y
+    return split_x_y
 
 
 # simple nan remover
