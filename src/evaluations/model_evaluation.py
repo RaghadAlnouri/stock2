792c88a09a42605934f2a1865aea6174e2b56c3f
Michael Al Tork <michael.altork@gmail.com>
2022-10-22 22:34:41 -0400

    edit evaluation python file

--- src/evaluations/model_evaluation.py
+++ src/evaluations/model_evaluation.py
@@ -3,7 +3,7 @@ import pandas as pd
 import yahoo_fin.stock_info as si
 from sklearn.metrics import balanced_accuracy_score
 
-from algo import create_pipeline
+from src.algo import create_pipeline
 
 
 def create_eval_data_fetcher(ticker: str, train: bool = True) -> Callable[[pd.DataFrame], pd.DataFrame]:
@@ -20,17 +20,16 @@ def create_eval_data_fetcher(ticker: str, train: bool = True) -> Callable[[pd.Da
 
 
 def main():
-    df = pd.read_csv('evaluations/sp500_stocks_data.csv', index_col=0, parse_dates=True)
+    df = pd.read_csv('src/evaluations/sp500_stocks_data.csv', index_col=0, parse_dates=True)
     sp500 = si.tickers_sp500()
     train_predictions = []
     test_predictions = []
     # for ticker in sp500:
     train_data_fetcher = create_eval_data_fetcher('AAPL')
     test_data_fetcher = create_eval_data_fetcher('AAPL', train=False)
-    train_predictor = create_pipeline.create_predictor('AAPL', train_data_fetcher, train_data_fetcher)
-    test_predictor = create_pipeline.create_predictor('AAPL', train_data_fetcher, test_data_fetcher)
-    print(train_predictor('AAPL'))
-
+    train_predictor = create_pipeline.create_predictor(df, train_data_fetcher, train_data_fetcher)
+    test_predictor = create_pipeline.create_predictor(df, train_data_fetcher, test_data_fetcher)
+    train_predictor(df)
 
 if __name__ == "__main__":
     main()
